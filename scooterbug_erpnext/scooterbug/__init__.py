1d482917ffb6e017a1bc616afd0036849efa6664
Ryan W <ryanw@bookingzone.com>
2025-12-25 04:03:32 -0500

    Fix module structure: rename nested module folder to scooterbug_erpnext to match Frappe's expected import path

--- scooterbug_erpnext/scooterbug/__init__.py
+++
@@ -1,4 +0,0 @@
-# ScooterBug Module
-# This module contains all ScooterBug-specific doctypes and functionality
-
-__version__ = "1.0.0"
---
+++ scooterbug_erpnext/scooterbug_erpnext/__init__.py
@@ -0,0 +1,3 @@
+"""ScooterBug ERPNext Module - Equipment Rental Management"""
+
+__version__ = "0.0.1"
